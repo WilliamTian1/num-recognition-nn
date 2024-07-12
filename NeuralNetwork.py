9cd91abaa1627e658c5168482546c0185175ec70
WilliamTian1 <53712338+WilliamTian1@users.noreply.github.com>
2024-07-12 16:03:59 -0400

    Implemented backpropogation and gradient descent to training method. Converted parameters to 2D arrays in query and train methods.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,25 +5,33 @@ class NeuralNetwork:
         self.inodes = inputNodes
         self.hnodes =  hiddenNodes
         self.onodes = outputNodes
-        self.lr = learningRate
+        self.lr = learningrate
         self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5),(self.hnodes, self.inodes))
         self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hodes))
         pass
     def train(self, inputs_list, targets_list): 
-        #convert inputs_list into 2D array for matrix multiplication
+        #convert inputs_list and target_list into 2D array for matrix multiplication
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
         hiddenInputs = numpy.dot(self.wih, inputs)
         self.activation = lambda x: scipy.special.expit(x)
-        hiddenOuputs = self.activation(hiddenInputs)
+        hiddenOutputs = self.activation(hiddenInputs)
         outputInputs = numpy.dot(self.who, hiddenOutputs)
         outputOutputs = self.activation(outputInputs)
         outputErrors = targets - outputOutputs
-
+        hiddenErrors =numpy.dot(self.who, outputErrors)
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((outputErrors * outputOutputs * (1.0 -
+        outputOutputs)), numpy.transpose(hiddenOutputs))
+        self.wih += self.lr * numpy.dot((hiddenErrors*hiddenOutputs*(1.0 - hiddenOutputs)),
+        numpy.transpose(hiddenInputs))
         pass
     def query(self, inputs_list):
         #convert inputs_list into 2D array for matrix multiplication
+        inputs = numpy.array(inputs_list, ndmin=2).T
         hiddenInputs = numpy.dot(self.wih, inputs)
         self.activation = lambda x: scipy.special.expit(x)
-        hiddenOuputs = self.activation(hiddenInputs)
+        hiddenOutputs = self.activation(hiddenInputs)
         outputInputs = numpy.dot(self.who, hiddenOutputs)
         outputOutputs = self.activation(outputInputs)
         return outputOutputs
